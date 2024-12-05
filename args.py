f00ffd48a0f75d25b7241d5c9baf877f207abab3
D.Y. Cheng <24451352007@stu.wzu.edu.cn>
2024-12-05 19:45:14 +0800

    First commit.

---
+++ args.py
@@ -0,0 +1,10 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username',type=str,default="",help="your user name.")
+    parser.add_argument('--passwd',type=str,default="",help="your password.")
+    parser.add_argument('--login_host',type=str,default="",help="login host.")
+    parser.add_argument('--ip',type=str,default="",help="login host.")
+    args = parser.parse_args()
+    return args
---
+++ login.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR && <your python execution path> $SCRIPT_DIR/login.py --username= --passwd= --login_host=
+
